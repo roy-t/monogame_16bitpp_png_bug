19d16baf6849240752dfe9d65809e0e0eac57cb2
Roy Triesscheijn <roy-t@hotmail.com>
2018-08-30 17:44:41 +0200

    Initial commit

--- PngBitDepth/Game1.cs
+++ PngBitDepth/Game1.cs
@@ -9,73 +9,35 @@ namespace PngBitDepth
     /// </summary>
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private Texture2D eightBitsPerPixelTexture;
+        private Texture2D sixteenBitsPerPixelTexture;
+
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+        }        
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+            this.eightBitsPerPixelTexture = this.Content.Load<Texture2D>("8bitpp");
+            this.sixteenBitsPerPixelTexture = this.Content.Load<Texture2D>("16bitpp");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            this.spriteBatch.Begin();
+
+            this.spriteBatch.Draw(this.eightBitsPerPixelTexture, new Rectangle(16, 16, 256, 256), null, Color.White);
+            this.spriteBatch.Draw(this.sixteenBitsPerPixelTexture, new Rectangle(16 + 16 + 256, 16, 256, 256), null, Color.White);
+
+            this.spriteBatch.End();
 
             base.Draw(gameTime);
         }
